855afde5b2e2c63b0e9de3502054a20c55504542
seeisalty <andymoore0204@gmail.com>
2025-04-10 11:39:28 -0500

    testing and fixes to make tests work delete account is still broken

--- intro_SE_lab/urls.py
+++ intro_SE_lab/urls.py
@@ -1,32 +1,14 @@
-"""
-URL configuration for intro_SE_lab project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from userauth import views as auth_views 
 from django.shortcuts import redirect
+from home import views as home_views
 
 urlpatterns = [
-    path('', lambda request: redirect('login')),  # ✅ Root redirects to /login/
-    path('sign-up/', include('userauth.urls')),
-    path('login/', include('userauth.urls')),
-    path('logout/', include('userauth.urls')),
-    path('home/', include('home.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('storefront.urls')),
+    path('', home_views.homepage_view, name='homepage'),  # Points to the homepage view in the home app
+    path('sign-up/', include('userauth.urls')),  # Make sure this includes the userauth urls
+    path('login/', include('userauth.urls')),  # Login URL
+    path('logout/', include('userauth.urls')),  # Logout URL
+    path('home/', include('home.urls')),  # Home page (after login)
+    path('storefront/', include('storefront.urls')),  # Storefront URL
+    path('admin/', admin.site.urls),  # Admin panel
 ]
-
-
