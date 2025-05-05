81cc1b9a4a37b624e711f8b12afc0569b62ee229
AidanScahill <aidancontrolled@gmail.com>
2025-05-05 01:01:14 -0500

    done

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -1,5 +1,5 @@
 # This file must be used with "source <venv>/bin/activate.fish" *from fish*
-# (https://fishshell.com/). You cannot run it directly.
+# (https://fishshell.com/); you cannot run it directly.
 
 function deactivate  -d "Exit virtual environment and return to normal shell environment"
     # reset old environment variables
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/mnt/c/Users/15854/ISE Project/Intro_to_SE_lab/venv'
+set -gx VIRTUAL_ENV '/mnt/c/Users/aidan/ItSE Local Branch/Intro_to_SE_lab/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
